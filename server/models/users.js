1587d1e77d2ec10606d8b496c6218a90fe1bd24b
ASUS <ps749924>
2023-08-12 16:23:04 +0545

    uppercase and lowercase

--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,13 @@
 const mongoose = require("mongoose");
+const passwordValidator = require('password-validator');
+
+// Create a password schema with complexity requirements
+const passwordSchema = new passwordValidator();
+passwordSchema
+   
+  .has().uppercase() // Must have at least one uppercase letter
+  .has().lowercase() // Must have at least one lowercase letter
+   
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,6 +26,10 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      validate: {
+        validator: (password) => passwordSchema.validate(password),
+        message: 'Password must meet complexity requirements: 8-12 characters, at least one uppercase, one lowercase, one digit, one special character.'
+      }
     },
     userRole: {
       type: String,
@@ -30,8 +43,8 @@ const userSchema = new mongoose.Schema(
       default: "user.png",
     },
     verified: {
-      type: String,
-      default: false,
+      type: Boolean, // Change to Boolean
+      default: false, // Default value is a boolean
     },
     secretKey: {
       type: String,
